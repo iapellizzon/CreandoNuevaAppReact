dea3d2804bdbb1737d91291ce4e30b6ee1d6d5ce
iapellizzon <iapellizzon@gmail.com>
2022-11-07 18:48:45 -0300

    Ultima actualizacion

--- src/componentes/NavBar/NavBar.js
+++ src/componentes/NavBar/NavBar.js
@@ -3,7 +3,7 @@ import CartWidgets from "../CartWidgets/CartWidgets";
 import "./NavBar.css";
 
 const NavBar = () => {
-  return (
+  /*  return (
     <div className="menu-navbar-conteiner">
       <h1 className="menu-title">Bicicletas Devenir</h1>
       <ul className="menu-items">
@@ -20,6 +20,31 @@ const NavBar = () => {
       </ul>
       <CartWidgets />
     </div>
+  ); */
+  return (
+    <nav className="navbar navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        Bicicletas Devenir
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item"> Bicicletas</li>
+          </ul>
+
+          <CartWidgets />
+        </div>
+      </div>
+    </nav>
   );
 };
 
