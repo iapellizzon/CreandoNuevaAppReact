ea02101ff337e082d345bd40df58431b04b88277
iapellizzon <iapellizzon@gmail.com>
2022-12-19 17:31:23 -0300

    los ultimos cambios

--- src/componentes/Item/item.js
+++ src/componentes/Item/item.js
@@ -1,14 +1,12 @@
+import { computeHeadingLevel } from "@testing-library/react";
 import { Link } from "react-router-dom";
 
 const Item = ({ product }) => {
+  console.log(product);
   return (
     <Link to={`/item/${product.id}`}>
       <div style={{ border: "1px solid red" }}>
-        <img
-          width={"300px"}
-          alt={product.title}
-          src={`/images/${product.imageId}`}
-        />
+        <img width={"300px"} alt={product.title} src={product.imageId} />
         <h2>{product.title}</h2>
         <h2>{product.description}</h2>
         <h2>{product.price}</h2>
